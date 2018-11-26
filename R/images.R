1b04ea255e57a2ad10dc2ed6a940bd3e8f270e2d
François Michonneau <francois.michonneau@gmail.com>
2018-11-26 16:34:36 -0500

    begin dealing with images

---
+++ R/images.R
@@ -0,0 +1,51 @@
+
+extract_img_html <- function(doc) {
+
+  doc <- normalizePath(doc)
+  base_path <- dirname(doc)
+
+  all_imgs <- xml2::read_html(doc) %>%
+    xml2::xml_find_all(".//img")
+
+  srcs <- purrr::map(all_imgs,
+                     ~ xml2::xml_find_all(., "@src") %>%
+                       xml2::xml_text()) %>%
+    purrr::map_chr(function(.x) {
+      if (length(.x) == 0L) return(NA_character_)
+      .x
+    })
+
+  alt_txt <- purrr::map(all_imgs,
+                        ~ xml2::xml_find_all(., "@alt") %>%
+                          xml2::xml_text()
+                        ) %>%
+    purrr::map_chr(function(.x) {
+      if (length(.x) == 0L) return(NA_character_)
+      .x
+    })
+
+
+  tibble::tibble(
+    src = srcs,
+    alt = alt_txt
+  )
+
+}
+
+
+check_images <- function(dir = ".", recursive = TRUE,
+                         regexp = "html?$", glob = NULL,
+                         ...) {
+
+  images <- fs::dir_ls(
+    path = dir,
+    recursive = recursive,
+    regexp = regexp,
+    glob = glob,
+    ...
+  ) %>%
+    purrr::map_df(extract_img_html, .id = "file")
+
+  images
+
+}
