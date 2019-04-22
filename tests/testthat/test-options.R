87038a390924f13ae3194f630bc0e48cf8df6ea9
François Michonneau <francois.michonneau@gmail.com>
2019-04-22 16:28:04 -0400

    update tests

--- tests/testthat/test-options.R
+++ tests/testthat/test-options.R
@@ -2,17 +2,17 @@ context("modify defaults")
 
 test_that("modify defaults with .Rprofile", {
   res <- withr::with_options(
-    list(checker.options = list(status_code_404 = TRUE)), {
+    list(checker.options = list(status_code_404 = 1L)), {
       check_links(dir = "html_files", regexp = "quick_broken.html",
         show_summary = FALSE, only_with_issues = FALSE)
     })
-  expect_true(res$valid[res$link_text == "external broken link"])
+  expect_true(res$error_level[res$link_text == "external broken link"] == 1L)
 })
 
 
 test_that("modify defaults with argument", {
   res <- check_links(dir = "html_files", regexp = "quick_broken.html",
     show_summary = FALSE, only_with_issues = FALSE,
-    checker_options = list(status_code_404 = TRUE))
-  expect_true(res$valid[res$link_text == "external broken link"])
+    checker_options = list(status_code_404 = 1L))
+  expect_true(res$error_level[res$link_text == "external broken link"] == 1L)
 })
