222be72bc5ef3ba582d19358569212673d91fa8c
François Michonneau <francois.michonneau@gmail.com>
2019-04-22 16:28:04 -0400

    update tests

--- tests/testthat/test_rel_paths.R
+++ tests/testthat/test_rel_paths.R
@@ -7,8 +7,8 @@ res_rel_paths <- check_links(
 )
 
 test_that("only broken links are broken", {
-  sub_valid  <- res_rel_paths[res_rel_paths$valid, ]
-  sub_broken <- res_rel_paths[!res_rel_paths$valid, ]
+  sub_valid  <- res_rel_paths[res_rel_paths$error_level == -1L, ]
+  sub_broken <- res_rel_paths[res_rel_paths$error_level ==  3L, ]
   expect_true(all(!grepl("broken", sub_valid$link_text)))
   expect_true(all(grepl("broken", sub_broken$link_text)))
 })
