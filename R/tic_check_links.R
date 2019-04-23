1ea7663c700fb2e13f562ba5effa06b26a99d715
François Michonneau <francois.michonneau@gmail.com>
2019-04-23 17:35:05 -0400

    test R6

--- R/tic_check_links.R
+++ R/tic_check_links.R
@@ -14,29 +14,44 @@ check_jekyll_links <- function(site_root = ".",
                                jekyll_port = "4000",
                                timeout = 1000,
                                verbose = TRUE,
-                               ...
+                               recursive = TRUE,
+                               only_with_issues = FALSE,
+                               show_summary = TRUE
                                ) {
-  CheckJekyllLinks$new(site_root, jekyll_port, timeout, verbose, ...)
+  CheckJekyllLinks$new(site_root, jekyll_port, timeout, verbose,
+    recursive, only_with_issues, show_summary)
 }
 
 
 CheckJekyllLinks <- R6::R6Class(
   "CheckJekyllLinks", inherit = tic::TicStep,
   public = list(
-    run = function(site_root, jekyll_port, timeout, verbose, ...) {
+    initialize = function(site_root, jekyll_port, timeout, verbose,
+                          recursive, only_with_issues, show_summary) {
+      private$site_root <- site_root
+      private$jekyll_port <- jekyll_port
+      private$timeout <- timeout
+      private$verbose <- verbose
+      private$recursive <- recursive
+      private$only_with_issues <- only_with_issues
+      private$show_summary <- show_summary
 
-      timeout <- as.difftime(timeout, units = "secs")
+      super$înitialize()
+    },
+    run = function() {
+
+      timeout <- as.difftime(private$timeout, units = "secs")
       deadline <- Sys.time() + timeout
 
-      bundle_install <- withr::with_dir(site_root, {
+      bundle_install <- withr::with_dir(private$site_root, {
         processx::run("bundle", "install")
       })
-      if (verbose) message(bundle_install$stdout)
+      if (private$verbose) message(bundle_install$stdout)
 
-      jkyl <- withr::with_dir("_rendered", {
+      jkyl <- withr::with_dir(private$site_root, {
         processx::process$new(
           "bundle",
-          c("exec", "jekyll", "serve", "--port", jekyll_port),
+          c("exec", "jekyll", "serve", "--port", private$jekyll_port),
           stdout = "|", stderr = "|")
       })
 
@@ -44,9 +59,9 @@ CheckJekyllLinks <- R6::R6Class(
         poll_time <- as.double(deadline - now, units = "secs") * 1000
         jkyl$poll_io(as.integer(poll_time))
         lines <- jkyl$read_output_lines()
-        if (verbose) message(cat(lines, sep = "\n"))
+        if (private$verbose) message(cat(lines, sep = "\n"))
         if (any(grepl("server running", lines, ignore.case = TRUE))) {
-          if (verbose) message("Jekyll is running just fine.")
+          if (private$verbose) message("Jekyll is running just fine.")
           break
         }
       }
@@ -54,10 +69,22 @@ CheckJekyllLinks <- R6::R6Class(
       on.exit(jkyl$kill(), add = TRUE)
 
       res_jekyll <- check_links(
-        file.path(site_root, "_site"),
-        root_dir = paste0("http://localhost:", jekyll_port),
-        ...
+        file.path(private$site_root, "_site"),
+        root_dir = paste0("http://localhost:", private$jekyll_port),
+        recursive = private$recursive,
+        only_with_issues = private$only_with_issues,
+        show_summary = private$show_summary
       )
 
-    })
+    }),
+
+  private = list(
+    site_root = NULL,
+    jekyll_port = NULL,
+    timeout = NULL,
+    verbose = NULL,
+    recursive = NULL,
+    only_with_issues = NULL,
+    show_summary = NULL
+  )
 )
