ba3553dadcb862b4a5099be4cb3ca4b1dddd0445
François Michonneau <francois.michonneau@gmail.com>
2019-04-23 17:39:15 -0400

    use simple function

--- R/tic_check_links.R
+++ R/tic_check_links.R
@@ -13,78 +13,42 @@
 check_jekyll_links <- function(site_root = ".",
                                jekyll_port = "4000",
                                timeout = 1000,
-                               verbose = TRUE,
-                               recursive = TRUE,
-                               only_with_issues = FALSE,
-                               show_summary = TRUE
+                               verbose,
+                               ...
                                ) {
-  CheckJekyllLinks$new(site_root, jekyll_port, timeout, verbose,
-    recursive, only_with_issues, show_summary)
-}
 
+  timeout <- as.difftime(timeout, units = "secs")
+  deadline <- Sys.time() + timeout
 
-CheckJekyllLinks <- R6::R6Class(
-  "CheckJekyllLinks", inherit = tic::TicStep,
-  public = list(
-    initialize = function(site_root, jekyll_port, timeout, verbose,
-                          recursive, only_with_issues, show_summary) {
-      private$site_root <- site_root
-      private$jekyll_port <- jekyll_port
-      private$timeout <- timeout
-      private$verbose <- verbose
-      private$recursive <- recursive
-      private$only_with_issues <- only_with_issues
-      private$show_summary <- show_summary
+  bundle_install <- withr::with_dir(site_root, {
+    processx::run("bundle", "install")
+  })
+  if (verbose) message(bundle_install$stdout)
 
-      super$înitialize()
-    },
-    run = function() {
+  jkyl <- withr::with_dir(site_root, {
+    processx::process$new(
+      "bundle",
+      c("exec", "jekyll", "serve", "--port", jekyll_port),
+      stdout = "|", stderr = "|")
+  })
 
-      timeout <- as.difftime(private$timeout, units = "secs")
-      deadline <- Sys.time() + timeout
+  while (jkyl$is_alive() && (now <- Sys.time()) < deadline) {
+    poll_time <- as.double(deadline - now, units = "secs") * 1000
+    jkyl$poll_io(as.integer(poll_time))
+    lines <- jkyl$read_output_lines()
+    if (verbose) message(cat(lines, sep = "\n"))
+    if (any(grepl("server running", lines, ignore.case = TRUE))) {
+      if (verbose) message("Jekyll is running just fine.")
+      break
+    }
+  }
 
-      bundle_install <- withr::with_dir(private$site_root, {
-        processx::run("bundle", "install")
-      })
-      if (private$verbose) message(bundle_install$stdout)
+  on.exit(jkyl$kill(), add = TRUE)
 
-      jkyl <- withr::with_dir(private$site_root, {
-        processx::process$new(
-          "bundle",
-          c("exec", "jekyll", "serve", "--port", private$jekyll_port),
-          stdout = "|", stderr = "|")
-      })
-
-      while (jkyl$is_alive() && (now <- Sys.time()) < deadline) {
-        poll_time <- as.double(deadline - now, units = "secs") * 1000
-        jkyl$poll_io(as.integer(poll_time))
-        lines <- jkyl$read_output_lines()
-        if (private$verbose) message(cat(lines, sep = "\n"))
-        if (any(grepl("server running", lines, ignore.case = TRUE))) {
-          if (private$verbose) message("Jekyll is running just fine.")
-          break
-        }
-      }
-
-      on.exit(jkyl$kill(), add = TRUE)
-
-      res_jekyll <- check_links(
-        file.path(private$site_root, "_site"),
-        root_dir = paste0("http://localhost:", private$jekyll_port),
-        recursive = private$recursive,
-        only_with_issues = private$only_with_issues,
-        show_summary = private$show_summary
-      )
-
-    }),
-
-  private = list(
-    site_root = NULL,
-    jekyll_port = NULL,
-    timeout = NULL,
-    verbose = NULL,
-    recursive = NULL,
-    only_with_issues = NULL,
-    show_summary = NULL
+  res_jekyll <- check_links(
+    file.path(site_root, "_site"),
+    root_dir = paste0("http://localhost:", jekyll_port),
+    ...
   )
-)
+  invisible(res_jekyll)
+}
