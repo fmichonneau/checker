fec659fc92afda039f2466f3cf79808aa039b1d3
François Michonneau <francois.michonneau@gmail.com>
2019-04-23 16:55:13 -0400

    write a tic function

---
+++ R/tic_check_links.R
@@ -0,0 +1,63 @@
+##' @title Check links on Travis
+##'
+##' Supports only Jekyll-based sites for now
+##'
+##' @param site_root path for the site
+##' @param jekyll_port which port to use
+##' @param timeout how long to wait for jekyll to sucessfully build the site (in
+##'   seconds)
+##' @param verbose should information about progress of the jekyll build be
+##'   displayed?
+##' @param ... additional arguments to be passed to check_links
+##' @export
+check_jekyll_links <- function(site_root = ".",
+                               jekyll_port = "4000",
+                               timeout = 1000,
+                               verbose = TRUE,
+                               ...
+                               ) {
+  CheckJekyllLinks$new(site_root, jekyll_port, timeout, verbose, ...)
+}
+
+
+CheckJekyllLinks <- R6::R6Class(
+  "CheckJekyllLinks", inherit = tic::TicStep,
+  public = list(
+    run = function(site_root, jekyll_port, timeout, verbose, ...) {
+
+      timeout <- as.difftime(timeout, units = "secs")
+      deadline <- Sys.time() + timeout
+
+      bundle_install <- withr::with_dir(site_root, {
+        processx::run("bundle", "install")
+      })
+      if (verbose) message(bundle_install$stdout)
+
+      jkyl <- withr::with_dir("_rendered", {
+        processx::process$new(
+          "bundle",
+          c("exec", "jekyll", "serve", "--port", jekyll_port),
+          stdout = "|", stderr = "|")
+      })
+
+      while (jkyl$is_alive() && (now <- Sys.time()) < deadline) {
+        poll_time <- as.double(deadline - now, units = "secs") * 1000
+        jkyl$poll_io(as.integer(poll_time))
+        lines <- jkyl$read_output_lines()
+        if (verbose) message(cat(lines, sep = "\n"))
+        if (any(grepl("server running", lines, ignore.case = TRUE))) {
+          if (verbose) message("Jekyll is running just fine.")
+          break
+        }
+      }
+
+      on.exit(jkyl$kill(), add = TRUE)
+
+      res_jekyll <- check_links(
+        file.path(site_root, "_site"),
+        root_dir = paste0("http://localhost:", jekyll_port),
+        ...
+      )
+
+    })
+)
