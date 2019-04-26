38d46b58875960dc2dd8c800d89e11c9805b1917
François Michonneau <francois.michonneau@gmail.com>
2019-04-26 14:46:36 -0400

    add tests for data-checker-ignore

---
+++ tests/testthat/test-checker-ignore.R
@@ -0,0 +1,15 @@
+context("make sure that data-checker-ignore attributes are being ignored")
+
+
+test_that("data-checker-ignore works", {
+  res_ignore_attr <- check_links(
+    dir = "./html_files",
+    regexp = "test_checker_ignore.html",
+    show_summary = FALSE,
+    only_with_issues = FALSE
+  )
+
+  expect_true(all(res_ignore_attr$error_level == -1L))
+  expect_true(all(!grepl("should be ignored",
+    res_ignore_attr$link_text, ignore.case = TRUE)))
+})
