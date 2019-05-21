ea9738d2ac5a196b2b91d4ee4fb186a9de6da4bb
François Michonneau <francois.michonneau@gmail.com>
2019-05-21 12:09:39 +0200

    [fix #34] test that function-provided options override options()

--- tests/testthat/test-options.R
+++ tests/testthat/test-options.R
@@ -16,3 +16,16 @@ test_that("modify defaults with argument", {
     checker_options = list(status_code_404 = "ok"))
   expect_true(res$error_level[res$link_text == "external broken link"] == "ok")
 })
+
+test_that("function-specified options override options()", {
+  expect_warning(res <- withr::with_options(
+    list(checker.options = list(status_code_404 = "ok")), {
+      check_links(
+        dir = "html_files",
+        regexp = "quick_broken.html",
+        show_summary = FALSE, only_with_issues = FALSE,
+        checker_options = list(status_code_404 = "warning")
+      )
+    })
+  )
+})
